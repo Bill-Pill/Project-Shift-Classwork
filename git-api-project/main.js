6e084215fad1396be9afeb36b8191b66fb1ef6dd
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-10-03 16:46:28 -0400

    Build base HTML layout

---
+++ git-api-project/main.js
@@ -0,0 +1,16 @@
+var fetchFace = function (query) {
+  var searchURL = "https://www.googleapis.com/books/v1/volumes?q=" + query
+
+
+  // $.ajax({
+  //   method: "GET",
+  //   url: searchURL,
+  //   dataType: "json",
+  //   success: function (data) {
+  //     app.addBooks(data);
+  //   },
+  //   error: function (jqXHR, textStatus, errorThrown) {
+  //     console.log(textStatus);
+  //   }
+  // });
+};
