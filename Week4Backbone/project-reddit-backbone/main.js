512f9ebe88134777c1fd2c9e1cff03cf548c3ab1
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-10-08 08:55:06 -0400

    10-8 classwork

---
+++ Week4Backbone/project-reddit-backbone/main.js
@@ -0,0 +1,35 @@
+var PostModel = Backbone.Model.extend({
+  defaults: function () {
+    return {
+      user: '',
+      text: ''
+    }
+  }
+});
+
+var PostView = Backbone.View.extend({
+  template: Handlebars.compile($('#post-template').html()),
+
+  className: 'post',
+
+  render: function () {
+    this.$el.html(this.template(this.model.attributes));
+
+    return this;
+  }
+});
+
+$('.add-post').on('click', function () {
+  var text = $('#post-name').val();
+  var user = $('#post-user').val();
+
+  var postModel = new PostModel({
+    text: text,
+    user: user
+  });
+  var postView = new PostView({
+    model: postModel
+  });
+
+  $('.posts').append(postView.render().el)
+});
